697bc082a4746cc8e55e6147b7a51105f1b7173c
IBMADMIN <53974957+harmeet2401@users.noreply.github.com>
2022-07-02 01:06:54 +0530

    Update ApiController.java

--- src/main/java/com/in28mins/Myapi/Controller/ApiController.java
+++ src/main/java/com/in28mins/Myapi/Controller/ApiController.java
@@ -29,7 +29,7 @@ public class ApiController {
     {
             User user = new User();
             user.setAdd("Plotno 63 wz 31 b3/8 ff navyug ");
-            user.setName("HARMEET SINGH - Phoenix Cell DT3 ");
+            user.setName("HARMEET SINGH - Fixers cell");
             return user;
     }
 }
